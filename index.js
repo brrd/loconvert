469cdac616061b7f64226005cc546fc79509c990
Thomas Brouard <contact@brrd.fr>
2021-03-31 18:41:35 +0200

    Search for soffice.exe once and for all

--- index.js
+++ index.js
@@ -3,18 +3,34 @@ const exec = util.promisify(require("child_process").exec);
 const path = require("path");
 const locatePath = require("locate-path");
 
-const loPaths = {
-  darwin: ["/Applications/LibreOffice.app/Contents/MacOS/soffice"],
-  linux: ["/usr/bin/libreoffice", "/usr/bin/soffice"],
-  win32: [
-    path.join(process.env["PROGRAMFILES(X86)"], "LIBREO~1/program/soffice.exe"),
-    path.join(process.env["PROGRAMFILES(X86)"], "LibreOffice/program/soffice.exe"),
-    path.join(process.env.PROGRAMFILES, "LibreOffice/program/soffice.exe"),
-  ]
-};
-
 const cwd = process.cwd();
+
 const getAbsolutePath = (p) => path.isAbsolute(p) ? p : path.join(cwd, p);
+
+let exePath;
+
+// FIXME: what if customPath changes between 2 executions?
+const getExePath = (customPath) => { 
+  if (exePath) {
+    return Promise.resolve(exePath);
+  }
+
+  const loPaths = {
+    darwin: ["/Applications/LibreOffice.app/Contents/MacOS/soffice"],
+    linux: ["/usr/bin/libreoffice", "/usr/bin/soffice"],
+    win32: [
+      path.join(process.env["PROGRAMFILES(X86)"], "LIBREO~1/program/soffice.exe"),
+      path.join(process.env["PROGRAMFILES(X86)"], "LibreOffice/program/soffice.exe"),
+      path.join(process.env.PROGRAMFILES, "LibreOffice/program/soffice.exe"),
+    ]
+  };
+  const loPath = customPath ? [customPath] : loPaths[process.platform];
+
+  return locatePath(loPath).then((locatedPath) => {
+    exePath = locatedPath;
+    return locatedPath;
+  });
+};
 
 function convert({document, format = "html", outdir, customPath}) {
   return new Promise((resolve, reject) => {
@@ -25,9 +41,8 @@ function convert({document, format = "html", outdir, customPath}) {
     const documentPath = getAbsolutePath(document);
     const outdirArg = outdir ? `--outdir "${getAbsolutePath(outdir)}"` : "";
     const args = ["--headless", `--convert-to ${format}`, outdirArg, `"${documentPath}"`];
-    const loPath = customPath ? [customPath] : loPaths[process.platform];
 
-    locatePath(loPath).then((exePath) => {
+    getExePath(customPath).then((exePath) => {
       const command = `"${exePath}" ${args.join(" ")}`;
       return exec(command, (error, stdout, stderr) => {
         if (error) {
